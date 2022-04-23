7def46b91436333f7c56da306f1b1fb94d911e92
deavator <deavator@gmail.com>
2022-04-23 17:28:18 +0300

    done lesson 2

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,26 @@
 function modal () {
+    const   modalForm = document.querySelector('.modal'),
+            modalBtn = document.querySelector('.modal__button');
+
+    // === ФУНКЦИИ ========================== 
+    function openModal () {
+        modalForm.style.display = "flex";
+    }
+    
+    function closeModal () {
+        modalForm.style.display = "none";
+    }
+
+    // === ОБРАБОТКА СОБЫТИЙ =================
+    modalBtn.addEventListener('click', () => {
+        openModal();
+    });
+
+    modalForm.addEventListener('click', (e) => {
+        const formContent = e.target.closest('.modal__inner');
+        if (!formContent) {closeModal();}
+    });
+
     
 }
 modal();
